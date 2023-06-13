d536a670712b7b99c7c66b55edf3717a0c706b7e
jpicados <98858767+jpicados@users.noreply.github.com>
2023-06-12 21:47:38 -0600

    SERVERTEST

---
+++ server.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";  // localhost
+$username = "admin";  // usuario de mysql
+$password = "admin";  // contrasena de mysql
+$database = "assetsdb";  // base de datos
+
+// crear coneccion
+$conn = new mysqli($servername, $username, $password, $database);
+
+// revisar coneccion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// select de activo
+$sql = "SELECT * FROM ";
+$result = $conn->query($sql);
+
+// fetch a un array
+$data = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+// enviar respuesta como json
+header("Content-Type: application/json");
+echo json_encode($data);
+
+// cerrar coneccion
+$conn->close();
+?>

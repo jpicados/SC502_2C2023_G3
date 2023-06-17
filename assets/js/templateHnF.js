b331dc9c53776e1e5822518ca3d64127d93fc4d5
Juan Leon <diego_leonl@outlook.com>
2023-06-17 01:36:18 -0600

    Template Header y Footer
    Se creo el template para el header y el footer usando JS y se cambiaron los colores del menu y del footer, se cambio la imagen de la pagina de inicio

---
+++ assets/js/templateHnF.js
@@ -0,0 +1,54 @@
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+header.innerHTML = `
+<div>
+<a href="index.html" class="logo">Intel Costa Rica</a> 
+</div>
+<nav class="navigation">
+<ul>
+  <li><a href="index.html">Inicio</a></li>
+  <li><a href="empleados.html">Empleados</a></li>
+  <li><a href="veractivos.html">Activos</a>
+    <ul>
+      <li>
+        <a href="agregaractivo.html">Agregar activos</a>
+        
+          </form>
+        </div>
+      </li>
+      <li><a href="veractivos.html">Ver activos </a></li>
+      <li><a href="empleados.html">Empleados</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Movimientos</a>
+    <ul>
+      <li><a href="#">Opcion 1</a></li>
+      <li><a href="#">Opcion 2</a></li>
+      <li><a href="#">Opcion 3</a></li>
+    </ul>
+    <li><a href="#">Perfil</a></li>
+  </li>
+  <li><a href="login.html">Cerrar Sesion</a></li>
+</ul>
+</nav>
+`;
+
+footer.innerHTML = `
+<div class="footer-info">
+<div class="contact-info">
+  <h3>Contacto</h3>
+  <p><strong>Teléfono:</strong>  (+506) 2298-6000</p>
+  <p><strong>Dirección:</strong> Componentes Intel de Costa Rica Calle 129 La Ribera de Belen Heredia, Costa Rica</p>
+</div>
+<div class="social-media">
+  <h3>Redes Sociales</h3>
+  <div class="social-icons">
+    <a href="#"><img src="../img/Logo_de_Facebook.png" alt="Facebook"></a>
+    <a href="#"><img src="../img/twitter-icon-free-png.webp" alt="Twitter"></a>
+    <a href="#"><img src="../img/instagram-icon.png" alt="Instagram"></a>
+  </div>
+</div>
+</div>
+<p>© 2023 Intel Costa Rica. Todos los derechos reservados.</p>
+`;
